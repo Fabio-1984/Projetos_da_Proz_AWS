044e2e51e4ec2179acd44dbff37470813d60c8f9
Fabio Navarro <80645214+Fabio-1984@users.noreply.github.com>
2023-10-21 16:23:54 -0300

    Update script.js
    Auterando o script

--- Aula_11.10_Eventos_Click/script.js
+++ Aula_11.10_Eventos_Click/script.js
@@ -1,6 +1,11 @@
+// Capiturar de elementos do DOM (variáveis)
+
 let quantidadeSubtotal = document.getElementById("quantidade-subtotal");
 let valorSubtotal = document.getElementById("valor-subtotal");
-
+let btnAdicionarProduto01 = document.querySelector('#btn-adicionar-produto-01');
+let quantidadeProduto01 = document.querySelector('#quantidade-produto-01');
+let btnSubtrairProduto01 = document.querySelector('#btn-subtrair-produto-01');
+let valorProduto01 = 11.66
 
 
 let subtotalInfo = {
@@ -8,41 +13,61 @@ let subtotalInfo = {
   valor: 11.66,
 };
 
-quantidadeSubtotal.innerText = subtotalInfo.quantidade + " itens";
-valorSubtotal.innerText = subtotalInfo.valor;
+// Definir a manipulação dos elementos do capiturados(funções)
 
-let btnAdicionarProduto01 = document.querySelector('#btn-adicionar-produto-01');
+function atualizarSubtotal() {
+    quantidadeSubtotal.innerText = subtotalInfo.quantidade + " itens";
+    valorSubtotal.innerText = subtotalInfo.valor.toFixed(2);
+    
+}
 
-
-
-let quantidadeProduto01 = document.querySelector('#quantidade-produto-01');
-
-
+atualizarSubtotal()
 
 function adicionarUm() {
+ // Adicionando 1 à quantidade do produto
 
   quantidadeProduto01.value = Number(quantidadeProduto01.value) + 1;
 
+   // Adicionando 1 à quantidade total
+
+  subtotalInfo.quantidade = subtotalInfo.quantidade + 1
+
+  // Adicionar o valor do produto subtotal
+
+   subtotalInfo.valor = subtotalInfo.valor + valorProduto01
+
+
+  //Atualizar o DOM
+  atualizarSubtotal()
+
 }
 
-
-
+//Definir quando devem ser manipulados os elementos (eventos)
 btnAdicionarProduto01.addEventListener('click', adicionarUm);
 
 
 
-let btnSubtrairProduto01 = document.querySelector('#btn-subtrair-produto-01');
-
-
-
 function subtrairUm() {
- if(quantidadeProduto01.value > 0){
-  quantidadeProduto01.value = Number(quantidadeProduto01.value) - 1;
- }
- 
+
+   // Adicionando 1 à quantidade do produto
+
+   quantidadeProduto01.value = Number(quantidadeProduto01.value) - 1;
+  
+   // Adicionando 1 à quantidade total
+
+   subtotalInfo.quantidade = subtotalInfo.quantidade - 1
+
+   // Adicionar o valor do produto subtotal
+
+   subtotalInfo.valor = subtotalInfo.valor - valorProduto01
 
 
+   //Atualizar o DOM
+  atualizarSubtotal()
+    
 }
+
+btnSubtrairProduto01.addEventListener('click', subtrairUm);
 
 
 
